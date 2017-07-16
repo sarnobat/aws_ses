ef6791cb8b87e7949e9fc2fbd58b094074a1a3f3
ssarnobat <ss401533@gmail.com>
2017-07-16 14:49:05 -0700

    Minor changes (untested)

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
+# Listening for network traffic
+nc -k -l 4468 | tee ~/github/aws_ses_subscriber/lastMessage.txt | tee -a ~/github/aws_ses_subscriber/log.txt |  perl -pe 's{\n}{}g' | perl -pe 's{POST.*Accept-Encoding: gzip,deflate}{}g' | jq -r '.["Subject"] + " :: " +  .["Timestamp"]' | tee -a ~/github/aws_ses_subscriber/test.txt 
 
 # To get the email file, execute:
 cat ~/github/aws_ses_subscriber/lastMessage.txt.json | jq '.Message | fromjson' | jq '.content '   | jq -r '.' | tee /Unsorted/trash/test.eml
